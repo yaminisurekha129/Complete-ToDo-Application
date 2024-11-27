21290cf82324bada3e57e11347f92d2e49d4c6cc
Dharani-Chandran <dharanichandran25@gmail.com>
2024-11-27 11:24:52 +0530

    This is first my git commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-task-app';
+
+  public items: { task: string, isEditing: boolean }[] = [];
+
+  public newTask: string='';
+
+  ngOnInit() {
+    const storedItems = localStorage.getItem('tasks');
+    if (storedItems) {
+      this.items = JSON.parse(storedItems);
+      
+    }
+  }
+
+  public addToList(){
+    if(this.newTask != '') {
+      this.items.push({task: this.newTask, isEditing:false});
+      this.newTask = '';
+      this.updateLocalStorage();
+    }
+  }
+
+  public deleteTask(index:number) {
+    this.items.splice(index, 1);
+    this.updateLocalStorage();
+  }
+
+  public editTask(index:number){
+    this.items[index].isEditing =!this.items[index].isEditing;
+
+  }
+
+  public saveEdit(index: number) {
+    this.items[index].isEditing = false;
+    this.updateLocalStorage();
+  }
+
+  private updateLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(this.items));
+  }
+
 }
